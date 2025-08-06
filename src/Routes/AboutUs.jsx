afbe57ab24fd7ba3af780ef3087754b1017ec202
=Val242 <=ebongvalentine70@gmail.com>
2025-08-06 19:37:20 +0100

    Building the About interface

--- src/Routes/AboutUs.jsx
+++ src/Routes/AboutUs.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import bgImage from '../assets/aboutbg.png'
 import logo from "../assets/Logo.png";
 import { NavLink } from "react-router-dom";
+import WTG from "../assets/WTG.png"
+import friends from "../assets/friends.png"
 
 function AboutUs() {
   return (
+    <div>
     <div  style={{
             backgroundImage: `url(${bgImage})`,
             backgroundSize: "cover",
@@ -30,9 +33,65 @@ function AboutUs() {
 
 
         </div>
+        
+          
+
 
 
       </div>
+     
+            
+          </div>
+          <div className='grid grid-cols-2 mb-10 gap-10 justify-center mt-10 ml-7 '>
+                               
+                                <div className='mx-auto ' style={{
+                          backgroundImage: `url(${WTG})`,
+                          backgroundSize: "cover"}}>
+                                  <h1 className='text-5xl font-sans'>
+                                    Hey, We are the Red Explorers Brief History<span className='text-orange-400 '>About Us </span>
+                                  </h1>
+                                </div>
+                                <div>
+                                  <p className='font-bold line leading-loose'>
+                                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mor mattis nec etiam ac. Bibendum tellus mi imperdiet amet maecenas magna tortor nulla. Nec tortor ut cursus ornare nibh vivamus. Quam elementum at velit viverra mattis.
+Eleifend enim, praesent eu, leo semper quis et. Nisl neque malesuada arcu, felis, vitae rhoncus morbi volutpat ante. Adipiscing mauris 
+          
+                                  </p>
+                                </div>
+                          
+                        
+                              </div>
+                          <div className='grid grid-cols-1 grid-rows-1 justify-center mx-8'>
+                                      <img src={friends} alt="" />
+                          </div>
+                          <div className='mt-10'>
+                            <p className='font-bold line leading-loose mx-8 text-[18px] pb-10'>
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis ut natoque imperdiet faucibus.
+                               Mi arcu, nunc imperdiet sit arcu id. In libero, nibh at malesuada nunc faucibus pulvinar dolor tellus. Rhoncus lectus hendrerit pharetra vitae magna. Pharetra, tortor pharetra vitae rhoncus. Nisl quam massa sapien tempor semper. Hac tempus pellentesque nibh duis ultrices. Senectus sagittis id ullamcorper mi eget pellentesque egestas. In ut sollicitudin ut orci volutpat egestas fermentum. Sit turpis diam risus leo bibendum neque, quis in vitae. 
+Praesent enim augue tellus vitae placerat purus pretium at massa. Aliquet a malesuada eget posuere. Pellentesque euismod nulla praesent rhoncus.
+                            </p>
+                          </div>
+                          <div className='grid grid-cols-2 grid-rows-2 gap-y-3 gap-x-6'>
+                            <div className='grid grid-rows-4 '>
+                              <div className=''>
+                                      <h1>Our Story</h1>
+                              </div>
+                                <div>
+                                  <p className='font-sans '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis ut natoque imperdiet faucibus. Mi arcu, nunc imperdiet sit arcu id. In libero, nibh at malesuada nunc faucibus. </p>
+                                </div>
+                                <div>
+                                  <p className='font-sans pb-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis ut natoque imperdiet faucibus. Mi arcu, nunc imperdiet sit arcu id. In libero, nibh at malesuada nunc faucibus pulvinar dolor tellus. Rhoncus lectus hendrerit pharetra vitae magna. Pharetra, tortor pharetra vitae rhoncus.</p>
+                                </div>
+                                <div>
+                                  <p className='font-sans pb-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis ut natoque imperdiet faucibus. Mi arcu, nunc imperdiet sit arcu id. In libero, nibh at malesuada nunc faucibus pulvinar dolor tellus. Rhoncus lectus hendrerit pharetra vitae magna. Pharetra, tortor pharetra vitae rhoncus.</p>
+                                </div>
+                                <div>
+                                  <p className='font-sans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis ut natoque imperdiet faucibus. Mi arcu, nunc imperdiet sit arcu id. In libero.</p>
+                                </div>
+                            </div>
+                                  
+
+                          </div>
           </div>
   )
 }
