a0f9367a140db42c9291579bcc09282e82f84e77
=Val242 <=ebongvalentine70@gmail.com>
2025-08-13 19:01:19 +0100

    Home Page In Progress

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,132 +1,91 @@
-import React from 'react'
-import bgImage from '../assets/contactbg.png'
+import React from 'react';
+import bgImage from '../assets/contactbg.png';
 import logo from "../assets/Logo.png";
 import { NavLink } from "react-router-dom";
-import nature from "../assets/nature.png"
-import lady from "../assets/lady.png"
-import map from "../assets/map.png"
-import plane from "../assets/plane.png"
-import woman from "../assets/woman.png"
-import mycrush from "../assets/mycrush.png"
-import water from "../assets/water.png"
-
-import { FiFacebook,FiTwitter,FiChrome} from 'react-icons/fi';
-import { FaLinkedin } from 'react-icons/fa6';
-import { FaRegCopyright } from 'react-icons/fa';
+import nature from "../assets/nature.png";
+import lady from "../assets/lady.png";
+import map from "../assets/map.png";
+import plane from "../assets/plane.png";
+import woman from "../assets/woman.png";
+import mycrush from "../assets/mycrush.png";
+import water from "../assets/water.png";
 
 import Footer from './Footer';
 
-<FaRegCopyright />
-
-
 function Contact() {
   return (
-    <div  style={{
-            backgroundImage: `url(${bgImage})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            height: "100vh",
-            width: "100%",
-          }}><div className="grid grid-cols-[1fr_2fr]  ml-3 mr-9 align-middle">
-                  <div className="">
-                   <img src={logo} alt="Logo" 
-                   
-                   className="w-[133px] h-[55.67px] "
-                   />
-          
-                  </div>
-                  <div className="text-[#FFA500] grid grid-cols-5 self-center  ">
-                   <div className="my-button w-[124px]"><NavLink to= "/"> Home</NavLink></div> 
-                   <div className="my-button w-[124px]"><NavLink to = "/aboutus">About Us</NavLink></div> 
-                   <div className="my-button w-[124px]"><NavLink to = "/destination">Destination</NavLink></div> 
-                   <div className="my-button w-[124px]"><NavLink to = "/tips">Tips</NavLink></div> 
-                   <div className="my-button w-[124px]"><NavLink to = "/contact">Contact</NavLink></div> 
-                  </div>
-            </div>
+    <div>
+    <div
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className="min-h-screen w-full"
+    >
+      {/* Navbar */}
+      <div className="flex flex-col md:flex-row justify-between items-center px-4 md:px-10 py-4">
+        <img src={logo} alt="Logo" className="w-[133px] h-[55.67px] mb-4 md:mb-0" />
+        <div className="text-[#FFA500] grid grid-cols-2 sm:grid-cols-5 gap-2">
+          <NavLink className="my-button" to="/">Home</NavLink>
+          <NavLink className="my-button" to="/aboutus">About Us</NavLink>
+          <NavLink className="my-button" to="/destination">Destination</NavLink>
+          <NavLink className="my-button" to="/tips">Tips</NavLink>
+          <NavLink className="my-button" to="/contact">Contact</NavLink>
+        </div>
+      </div>
 
-
-                <div className='grid place-items-center h-screen'>
-                  <h1 className='text-white font-bold text-6xl grid place-items-center h-screen'>
-                    Contact
-                  </h1>
-                </div>
-
-                <div className='grid place-items-center h-screen '>
-                  <div className=''>
-                    <h2 className='text-center mb-7'>PLEASE, FEEL YOURSELF COMFORTABLE TO FILL IN OUR CONTACT FORM</h2>
-                  </div>
-                  <form className='grid grid-rows-5 grid-cols-2 gap-5 w-[855px]'>
-                     <input type="text"  placeholder='First Name'
-                       className='grid  border border-[#9B9DB3] p-2'/>
-                     <input type="text"  placeholder='Last Name' 
-                       className='grid  border border-[#9B9DB3] p-2'/>
-                      <input type="text" placeholder='Email' 
-                       className='grid  border border-[#9B9DB3] p-2'/>
-                      <input type="text" placeholder='Subject'
-                        className='grid  border border-[#9B9DB3] p-2' />
-                      <input type="text" placeholder='Message'
-                       className='grid  border border-[#9B9DB3] p-2 row-span-3 col-span-2  ' />
-                        </form>
-                  
-                    <div className='grid justify-center mt-4'>
-                      <button className='bg-[#0336FF] rounded-2xl w-[135px] h-[33px]'>
-                       Submit
-                      </button>
-                        </div>
-                          </div>
-                            <div>
-                  <div className='grid grid-cols-7 grid-rows-1'>
-                        <div>
-                      <img src={nature} alt="nature" />
-                        </div>
-                        <div>
-                      <img src={lady} alt="lady" />
-                        </div>
-                        <div>
-                      <img src={map} alt="map" />
-                        </div>
-                         <div>
-                      <img src={plane} alt="plane" />
-                        </div>
-                       <div>
-                      <img src={woman} alt="woman" />
-                       </div>
-                       <div>
-                      <img src={mycrush} alt="mycrush" />
-                       </div>
-                       <div>
-                      <img src={water} alt="water" />
-                      </div>
-                </div>
-                  <div className='grid place-items-center grid-cols-[1fr_1fr] mt-5  border-b border-b-[#C4C4C4]  '>
-                          <div>
-                            <p className='font-bold text-3xl'>Join 98,641 Monthly Readers. <br /> Subscribe Today!</p>
-                          </div>
-                          <div className='grid grid-cols-[2fr_1fr] gap-x-2 '>
-                             <input type="email"  placeholder='Email Address'
-                          
-                          className='grid  border border-[#9B9DB3] p-2 rounded-[5px] '/>
-                          
-                          <div>
-                            <button className='bg-[#0336FF] w-[150px] h-[50px]  rounded-[5px]'>
-                              Subscribe
-                            </button>
-                          </div>
-                          </div>
-                  </div>
-      <Footer></Footer>
-              </div>
-
-
-
-
+      {/* Page Title */}
+      <div className="flex justify-center items-center py-16">
+        <h1 className="text-white font-bold text-4xl sm:text-5xl md:text-6xl">Contact</h1>
+      </div>
 </div>
-                
-                
-                
-                
-                
-  )
+      {/* Contact Form */}
+      <div className="px-4 sm:px-6 lg:px-10 max-w-4xl mx-auto">
+        <h2 className="text-center mb-7 text-base sm:text-lg md:text-xl font-medium">
+          PLEASE, FEEL YOURSELF COMFORTABLE TO FILL IN OUR CONTACT FORM
+        </h2>
+        <form className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <input type="text" placeholder="First Name" className="border border-[#9B9DB3] p-2" />
+          <input type="text" placeholder="Last Name" className="border border-[#9B9DB3] p-2" />
+          <input type="email" placeholder="Email" className="border border-[#9B9DB3] p-2" />
+          <input type="text" placeholder="Subject" className="border border-[#9B9DB3] p-2" />
+          <textarea placeholder="Message" className="border border-[#9B9DB3] p-2 sm:col-span-2 h-32" />
+        </form>
+        <div className="flex justify-center mt-4">
+          <button className="bg-[#0336FF] rounded-2xl px-6 py-2 text-white">
+            Submit
+          </button>
+        </div>
+      </div>
+
+      {/* Image Row */}
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-7 gap-2 px-4 mt-8">
+        {[nature, lady, map, plane, woman, mycrush, water].map((src, idx) => (
+          <img key={idx} src={src} alt={`img-${idx}`} className="w-full h-auto object-cover" />
+        ))}
+      </div>
+
+      {/* Subscribe Section */}
+      <div className="grid gap-4 md:grid-cols-2 items-center mt-8 border-b border-b-[#C4C4C4] px-4 py-6">
+        <p className="font-bold text-xl sm:text-2xl md:text-3xl text-center md:text-left">
+          Join 98,641 Monthly Readers. <br /> Subscribe Today!
+        </p>
+        <div className="flex flex-col sm:flex-row gap-2 justify-center md:justify-start">
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="border border-[#9B9DB3] p-2 rounded-[5px] flex-1"
+          />
+          <button className="bg-[#0336FF] text-white px-6 py-2 rounded-[5px]">
+            Subscribe
+          </button>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import bgImage from "../assets/bgImage.png"; // correct path from Routes/ to assets/
 import logo from "../assets/Logo.png";
 import { NavLink } from "react-router-dom";
+import { GoLocation } from "react-icons/go";
+import { FaCommentDots } from "react-icons/fa";
+ // Font Awesome
+// or import { BsChatDots } from "react-icons/bs"; // Bootstrap Icons
+
 import '../App.css'
 import Footer from "./Footer";
+import whiteWife from "../assets/whiteWife.png"
+import desert from "../assets/desert.png"
 
 export default function Home() {
   return (
@@ -24,7 +31,7 @@ export default function Home() {
          />
 
         </div>
-        <div className="text-[#FFA500] grid grid-cols-5 self-center  ">
+        <div className="text-[#FFA500] grid grid-cols-5 self-center sm:text-[10px]  ">
          <div className="my-button w-[124px]"><NavLink to= "/"> Home</NavLink></div> 
          <div className="my-button w-[124px]"><NavLink to = "/aboutus">About Us</NavLink></div> 
          <div className="my-button w-[124px]"><NavLink to = "/destination">Destination</NavLink></div> 
@@ -35,7 +42,12 @@ export default function Home() {
         </div>
 
 
+
       </div>
+
+       
+
+
         <div className="grid place-items-center h-screen">
       <div className="grid justify-center  ">
         <h2 className="text-white font-bold text-4xl"> THE COUNTER: <span className="text-[#FFA500]" >70</span> Countries <span className="text-[#FFA500]">1036</span> Cities </h2>
@@ -53,7 +65,100 @@ export default function Home() {
         <p className="text-white">Scroll Down to Continue</p>
       </div>
       </div>
-      <Footer></Footer>
+
+         <div className="display grid grid-cols-[1fr_2fr] grid-rows-20 border-1 border-gray-600 p-4 mt-8 gap-5">
+                
+              <div className="grid grid-rows-[0.1fr_0.5fr_0.2fr_0.3fr] mx-2 place-items-center w-[438.23px] border-1 border-gray-600">
+                    <div>
+                  <h2 className="text-center font-bold">About me</h2>
+                </div>
+                    <img src={whiteWife} alt="" className="w-[363.9px]"/>
+                    <div className="w-[363.9px]">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cum in vel massa donec sit. Mi ut risus sem malesuada ornare. Ac eu erat eget et lorem est arcu. Gravida hendrerit sit blandit semper lacus. Nulla amet suscipit sit lectus tortor. Dolor non eget suspendisse leo scelerisque sed d.
+                    </div>
+                    <div>
+                       <button className="bg-[#0336FF] w-[150px] h-[50px] ">
+          Read More
+        </button>
+                    </div>
+              </div>
+              <div className=" grid-rows-2 ">
+                <div><img src={desert} alt="" /></div>
+                <div className=" grid-rows-[1fr_3fr_3fr_1fr]">
+                    <div><p>July, 15, 2021 - Tips and Tricks </p></div>
+                    <div className="ml-2 mr-4"><h2 className="font-bold py-2">A traveler’s guide to Penang, Malaysia - Where to Eat, Drink, Sleep and Explore </h2></div>
+                    <div className="ml-2 mr-4"><p className=" pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra pharetra ac erat commodo non leo eget gravida viverra. Pharetra pharetra.</p></div>
+                    <div className="grid grid-cols-2 grid-rows-1 gap-100 ml-2">
+
+                      <div >
+                           <GoLocation className="text-[#FFA500] inline w-[25px] h-[25px]"></GoLocation>Penang, Malaysia 
+                        </div>
+                      <div className="">
+                            <FaCommentDots className="text-[#FFA500] inline w-[25px] h-[25px]"/>Comment (52)
+                        </div>
+
+                    </div>
+                </div>
+                  
+              </div>
+              <div className="border-2 border-black px-2 py-4 grid-rows-[1fr_4fr]">
+                  
+                      <div className="">
+                               <h2 className="text-center font-bold">Categories</h2>
+                      </div>
+                     
+
+                  
+                    <div className="grid grid-rows-7">
+                          <div className="mb-8 py-4 grid grid-cols-2 grid-rows-1 gap-100 ml-5">
+                           <p>Europe</p> 
+                           <p>(11)</p>
+                            </div>
+                          <div className="mb-8 py-4">Mountains</div>
+                          <div className="mb-8 py-4">Traveling Video</div>
+                          <div className="mb-8 py-4">Beauty of Seas</div>
+                          <div className="mb-8 py-4">Cuisine</div>
+                          <div className="mb-8 py-4">Guide of traveler</div>
+                          <div className="mb-8 py-4">Other</div>
+                      </div>
+              </div>
+              <div className="border-2 border-black">
+                  
+              </div>
+              <div className="border-2 border-black">
+
+              </div>
+              <div className="border-2 border-black">
+
+              </div>
+              <div className="border-2 border-black">
+
+              </div>
+              <div className="border-2 border-black">
+
+              </div>
+              <div className="border-2 border-black">
+
+              </div>
+              <div className="border-2 border-black">
+
+              </div>
+              <div className="border-2 border-black">
+
+              </div>
+              <div className="border-2 border-black">
+
+              </div>
+              <div className="border-2 border-black">
+
+              </div>
+            
+
+          
+         
+     
+    </div>
+     <Footer></Footer>
     </div>
   );
 }
