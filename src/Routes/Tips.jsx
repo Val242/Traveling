bdaf35f1f3305c82c5021b764f05883514452ecf
=Val242 <=ebongvalentine70@gmail.com>
2025-08-20 14:43:50 +0100

    Optimized tips code with prop reusability

--- src/Routes/Tips.jsx
+++ src/Routes/Tips.jsx
@@ -3,11 +3,6 @@ import bgImage from '../assets/tipsbg.png'
 import logo from "../assets/Logo.png";
 import { NavLink } from "react-router-dom";
 import goingWild from "../assets/goingWild.png"
-import { FaMapMarkerAlt } from "react-icons/fa";
-import { FaRegCommentDots } from "react-icons/fa";
-import marker from "../assets/Marker.png"
-import comment from "../assets/Communication.png"
-import star from "../assets/Star.png"
 import northPole from "../assets/northPole.png"
 import summerVibes from "../assets/summerVibes.png"
 import city from "../assets/city.png"
@@ -16,11 +11,8 @@ import beaches from "../assets/beaches.png"
 import trip101 from "../assets/trip101.png"
 import waterFront from "../assets/waterFront.png"
 import mywife from "../assets/blackBeauty.png"
-import bolo from "../assets/sideHustle.png"
-import { FiFacebook,FiTwitter,FiChrome} from 'react-icons/fi';
-import { FaLinkedin } from 'react-icons/fa6';
-import { FaRegCopyright } from 'react-icons/fa';
 import Footer from './Footer';
+import BlogCard from '../BlogCard';
 
 function Tips() {
   return (
@@ -77,220 +69,15 @@ function Tips() {
                       </div>
                 </div>
                 <div className='grid grid-cols-3 grid-rows-3 gap-10 place-items-center '>
-                  <div className='rounded-3xl overflow-hidden shadow-lg  place-items-center mx-auto'>
-                      <div>
-                        <img src={northPole} alt="" />
-                      </div>
-                      <div>
-                        <p>January 15, 2019 - Travel Tips</p>
-                      </div>
-                      <div className='bg-[#0336FF] w-100%'>
-                        <h2 className='text-white font-bold'>Going to the Extreme - The Northern Pole</h2>
-                      </div>
-                      <div className='grid grid-cols-3 place-items-center gap-3'>
-                        <div>
-                          <img src={marker} alt="Location" className='inline w-[30px] h-[30px]' /> North Pole
-                        </div>
-                        <div className='text-[15px]'>
-                          <img src={comment} alt="Comment" className='inline w-[20px] h-[20px]' /> 12 Comments
-                        </div>
-                        <div>
-                          <img src={star} alt="Rating" className='inline w-[25px] h-[25px]' /> 4.8 of 5
-                        </div>
-                      </div>
-                    </div>
-
-                      <div className='rounded-3xl overflow-hidden shadow-lg  place-items-center mx-auto'>
-                      <div>
-                        <img src={summerVibes} alt="" />
-                      </div>
-                      <div>
-                        <p>January 15, 2019 - Travel Tips</p>
-                      </div>
-                      <div className='bg-[#0336FF]'>
-                        <h2 className='text-white font-bold'>Going to the Extreme - The Northern Pole</h2>
-                      </div>
-                      <div className='grid grid-cols-3 place-items-center gap-3'>
-                        <div>
-                          <img src={marker} alt="Location" className='inline w-[30px] h-[30px]' /> North Pole
-                        </div>
-                        <div className='text-[15px]'>
-                          <img src={comment} alt="Comment" className='inline w-[20px] h-[20px]' /> 12 Comments
-                        </div>
-                        <div>
-                          <img src={star} alt="Rating" className='inline w-[25px] h-[25px]' /> 4.8 of 5
-                        </div>
-                      </div>
-                    </div>
-
-                      <div className='rounded-3xl overflow-hidden shadow-lg  place-items-center mx-auto'>
-                      <div>
-                        <img src={city} alt="" />
-                      </div>
-                      <div>
-                        <p>January 15, 2019 - Travel Tips</p>
-                      </div>
-                      <div className='bg-[#0336FF]'>
-                        <h2 className='text-white font-bold'>Going to the Extreme - The Northern Pole</h2>
-                      </div>
-                      <div className='grid grid-cols-3 place-items-center gap-3'>
-                        <div>
-                          <img src={marker} alt="Location" className='inline w-[30px] h-[30px]' /> North Pole
-                        </div>
-                        <div className='text-[15px]'>
-                          <img src={comment} alt="Comment" className='inline w-[20px] h-[20px]' /> 12 Comments
-                        </div>
-                        <div>
-                          <img src={star} alt="Rating" className='inline w-[25px] h-[25px]' /> 4.8 of 5
-                        </div>
-                      </div>
-                    </div>
-
-                      <div className='rounded-3xl overflow-hidden shadow-lg  place-items-center mx-auto'>
-                      <div>
-                        <img src={goingWild} alt="" />
-                      </div>
-                      <div>
-                        <p>January 15, 2019 - Travel Tips</p>
-                      </div>
-                      <div className='bg-[#0336FF]'>
-                        <h2 className='text-white font-bold'>Going to the Extreme - The Northern Pole</h2>
-                      </div>
-                      <div className='grid grid-cols-3 place-items-center gap-3'>
-                        <div>
-                          <img src={marker} alt="Location" className='inline w-[30px] h-[30px]' /> North Pole
-                        </div>
-                        <div className='text-[15px]'>
-                          <img src={comment} alt="Comment" className='inline w-[20px] h-[20px]' /> 12 Comments
-                        </div>
-                        <div>
-                          <img src={star} alt="Rating" className='inline w-[25px] h-[25px]' /> 4.8 of 5
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className='rounded-3xl overflow-hidden shadow-lg  place-items-center mx-auto'>
-                      <div>
-                        <img src={chocolate} alt="Chocolate" />
-                      </div>
-                      <div>
-                        <p>January 15, 2019 - Travel Tips</p>
-                      </div>
-                      <div className='bg-[#0336FF]'>
-                        <h2 className='text-white font-bold'>Going to the Extreme - The Northern Pole</h2>
-                      </div>
-                      <div className='grid grid-cols-3 place-items-center gap-3'>
-                        <div>
-                          <img src={marker} alt="Location" className='inline w-[30px] h-[30px]' /> North Pole
-                        </div>
-                        <div className='text-[15px]'>
-                          <img src={comment} alt="Comment" className='inline w-[20px] h-[20px]' /> 12 Comments
-                        </div>
-                        <div>
-                          <img src={star} alt="Rating" className='inline w-[25px] h-[25px]' /> 4.8 of 5
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className='rounded-3xl overflow-hidden shadow-lg  place-items-center mx-auto'>
-                      <div>
-                        <img src={beaches} alt="Beaches" />
-                      </div>
-                      <div>
-                        <p>January 15, 2019 - Travel Tips</p>
-                      </div>
-                      <div className='bg-[#0336FF]'>
-                        <h2 className='text-white font-bold'>Going to the Extreme - The Northern Pole</h2>
-                      </div>
-                      <div className='grid grid-cols-3 place-items-center gap-3'>
-                        <div>
-                          <img src={marker} alt="Location" className='inline w-[30px] h-[30px]' /> North Pole
-                        </div>
-                        <div className='text-[15px]'>
-                          <img src={comment} alt="Comment" className='inline w-[20px] h-[20px]' /> 12 Comments
-                        </div>
-                        <div>
-                          <img src={star} alt="Rating" className='inline w-[25px] h-[25px]' /> 4.8 of 5
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className='rounded-3xl overflow-hidden shadow-lg  place-items-center mx-auto'>
-                      <div>
-                        <img src={trip101} alt="Trip 101" />
-                      </div>
-                      <div>
-                        <p>January 15, 2019 - Travel Tips</p>
-                      </div>
-                      <div className='bg-[#0336FF]'>
-                        <h2 className='text-white font-bold'>Going to the Extreme - The Northern Pole</h2>
-                      </div>
-                      <div className='grid grid-cols-3 place-items-center gap-3'>
-                        <div>
-                          <img src={marker} alt="Location" className='inline w-[30px] h-[30px]' /> North Pole
-                        </div>
-                        <div className='text-[15px]'>
-                          <img src={comment} alt="Comment" className='inline w-[20px] h-[20px]' /> 12 Comments
-                        </div>
-                        <div>
-                          <img src={star} alt="Rating" className='inline w-[25px] h-[25px]' /> 4.8 of 5
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className='rounded-3xl overflow-hidden shadow-lg  place-items-center mx-auto'>
-                      <div>
-                        <img src={waterFront} alt="Water Front" />
-                      </div>
-                      <div>
-                        <p>January 15, 2019 - Travel Tips</p>
-                      </div>
-                      <div className='bg-[#0336FF]'>
-                        <h2 className='text-white font-bold'>Going to the Extreme - The Northern Pole</h2>
-                      </div>
-                      <div className='grid grid-cols-3 place-items-center gap-3'>
-                        <div>
-                          <img src={marker} alt="Location" className='inline w-[30px] h-[30px]' /> North Pole
-                        </div>
-                        <div className='text-[15px]'>
-                          <img src={comment} alt="Comment" className='inline w-[20px] h-[20px]' /> 12 Comments
-                        </div>
-                        <div>
-                          <img src={star} alt="Rating" className='inline w-[25px] h-[25px]' /> 4.8 of 5
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className='rounded-3xl overflow-hidden shadow-lg  place-items-center mx-auto'>
-                      <div>
-                        <img src={mywife} alt="Black Beauty" />
-                      </div>
-                      <div>
-                        <p>January 15, 2019 - Travel Tips</p>
-                      </div>
-                      <div className='bg-[#0336FF]'>
-                        <h2 className='text-white font-bold'>Going to the Extreme - The Northern Pole</h2>
-                      </div>
-                      <div className='grid grid-cols-3 place-items-center gap-3'>
-                        <div>
-                          <img src={marker} alt="Location" className='inline w-[30px] h-[30px]' /> North Pole
-                        </div>
-                        <div className='text-[15px]'>
-                          <img src={comment} alt="Comment" className='inline w-[20px] h-[20px]' /> 12 Comments
-                        </div>
-                        <div>
-                          <img src={star} alt="Rating" className='inline w-[25px] h-[25px]' /> 4.8 of 5
-                        </div>
-                      </div>
-                    </div>
-
-                     
-                   
-                     
-                    
-
-                   
-                  
+                   <BlogCard image={northPole} />
+                    <BlogCard image={summerVibes} />
+                    <BlogCard image={city} />
+                    <BlogCard image={goingWild} />
+                    <BlogCard image={chocolate} />
+                    <BlogCard image={beaches} />
+                    <BlogCard image={trip101} />
+                     <BlogCard image={waterFront} />
+                     <BlogCard image={mywife} />
 
                 </div>
                 <Footer></Footer>
